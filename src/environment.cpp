2be1fc1d50984b2f8958bb625cd7766dd6d7706e
Magnus Burenius <magnus.burenius@gmail.com>
2022-12-11 15:00:33 +0100

    refactor: environment

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,8 +1,8 @@
 #include "environment.hpp"
 
 Environment makeEnvironment(size_t screen_width, size_t screen_height) {
-    const auto map = Map();
-    auto environment = Environment(map);
+    auto environment = Environment{};
+    environment.map = Map{};
     environment.intrinsics = makeCameraIntrinsics(screen_width, screen_height);
     environment.extrinsics = makeCameraExtrinsics();
     return environment;
--- src/environment.hpp
+++ src/environment.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
-#include <string>
-#include <vector>
-
 #include "camera.hpp"
 #include "map.hpp"
 #include "timer.hpp"
-#include "vector_space.hpp"
 
 struct Environment {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    explicit Environment(const Map& map) : map{ map } {}
     CameraIntrinsics intrinsics;
     CameraExtrinsics extrinsics;
     Map map;
@@ -18,5 +13,3 @@ struct Environment {
 };
 
 Environment makeEnvironment(size_t screen_width, size_t screen_height);
-Vector4d cameraViewPosition(const CameraExtrinsics& coordinates);
-
